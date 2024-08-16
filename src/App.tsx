b0e1a957b5e570c14a28f413503b3f64ebab513b
anshuER <anshupatel710@gmail.com>
2024-08-16 17:46:50 +0530

    negative number error

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,37 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [numbers, setNumbers] = useState('');
+  let [numbers, setNumbers] = useState('');
+  const [total, setTotal] = useState(0);
+  const [error, setError] = useState('');
 
-  function add(numbers: string): number {
-    let delimiter = ',';
+  function add() {
+    try {
+      if (numbers === '') return 0;
+      let delimiter = ',';
 
-    if (numbers.startsWith('//')) {
-      const delimiterEndIndex = numbers.indexOf('\\n');
-      delimiter = numbers.substring(2, delimiterEndIndex);
-      numbers = numbers.substring(delimiterEndIndex + 2);
+      if (numbers.startsWith('//')) {
+        const delimiterEndIndex = numbers.indexOf('\\n');
+        delimiter = numbers.substring(2, delimiterEndIndex);
+        numbers = numbers.substring(delimiterEndIndex + 2);
+      }
+
+      const sanitizedNumbers = numbers.replace(/\\n/g, delimiter);
+      const extractedNumbers = sanitizedNumbers.split(',');
+
+      const negatives = extractedNumbers.filter((n) => +n < 0);
+      if (negatives.length > 0) {
+        setError(`negative numbers not allowed ${negatives.join(',')}`);
+      }
+
+      const total = extractedNumbers.reduce((acc, curr) => {
+        return acc + (parseFloat(curr) || 0);
+      }, 0);
+
+      setTotal(total);
+    } catch (error) {
+      console.log('error', error);
     }
-
-    const sanitizedNumbers = numbers.replace(/\\n/g, delimiter);
-    const extractedNumbers = sanitizedNumbers.split(',');
-
-    const total = extractedNumbers.reduce((acc, curr) => {
-      return acc + (parseFloat(curr) || 0);
-    }, 0);
-
-    return total;
   }
 
   return (
@@ -38,15 +50,22 @@ function App() {
           onChange={(e) => setNumbers(e.target.value)}
           className='w-full p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-purple-500 transition-all mb-4'
         />
+        <button
+          onClick={add}
+          className='w-full p-3 bg-purple-600 text-white font-bold rounded-lg hover:bg-purple-700 transition-all'
+        >
+          Calculate
+        </button>
         <div
           className={`text-lg font-semibold text-center text-purple-700 p-3 rounded-lg mt-4 transition-all duration-300 transform ${
-            add(numbers)
-              ? 'opacity-100 translate-y-0'
-              : 'opacity-0 translate-y-4'
+            total > 0 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
           }`}
         >
-          Total: {add(numbers)}
+          Total: {total}
         </div>
+        {error && total <= 0 && (
+          <p className='text-center text-red-500'>{error}</p>
+        )}
       </div>
     </div>
   );
